63e3ecec6f02909d7912fb02a6fa2200e728b0ce
Aurélie Ginioux <giniouxe@users.noreply.github.com>
2021-03-16 20:44:42 +0100

    Update installations.sh

--- installations.sh
+++ installations.sh
@@ -1,14 +1,11 @@
 # Terminator
-sudo apt-get update
-sudo apt-get install terminator
+sudo apt-get update && sudo apt-get install terminator
 
 # Git
 sudo apt-get install git
 
 # ZSH
-sudo apt-get install zsh
-zsh --version
-chsh -s $(which zsh)
+sudo apt-get install zsh && zsh --version && chsh -s $(which zsh)
 
 # curl
 sudo apt install curl
@@ -17,37 +14,25 @@ sudo apt install curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # ZSH autosuggestions
-mkdir ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+mkdir ~/.zsh/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 # Firefox
-sudo apt-get update
-sudo apt-get install firefox
+sudo apt-get update && sudo apt-get install firefox
 
 # Chromium
-sudo apt-get update
-sudo apt-get install chromium-browser
+sudo apt-get update && sudo apt-get install chromium-browser
 
 # Atom
-curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt-get update
-sudo apt-get install atom
-
-# Slack
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && sudo apt-get update && sudo apt-get install atom
 
 # PostgreSQL
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get update && sudo apt-get install postgresql postgresql-contrib
 
 # MySQL
-sudo apt-get update
-sudo apt-get install mysql-client
+sudo apt-get update && sudo apt-get install mysql-client
 
 # Libre office
-sudo add-apt-repository ppa:libreoffice/ppa
-sudo apt-get update
-sudo apt-get install libreoffice
+sudo add-apt-repository ppa:libreoffice/ppa && sudo apt-get update && sudo apt-get install libreoffice
 
 # ASDF
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
@@ -64,21 +49,13 @@ asdf global ruby 2.7.2
 sudo apt-get install rubygems
 
 # Bundler
-gem install bundler
-# Check
-bundler --version
+gem install bundler && bundler --version
 
 # Foreman
-gem install foreman
-# Check
-foreman --version
+gem install foreman && foreman --version
 
 # Elixir
-asdf plugin add erlang
-asdf install erlang 23.2
-asdf plugin add elixir
-asdf install elixir 1.10
-asdf global elixir 1.10
+asdf plugin add erlang && asdf install erlang 23.2 && asdf plugin add elixir && asdf install elixir 1.10 && asdf global elixir 1.10
 
 # Docker
 sudo apt-get install docker docker-compose
@@ -90,14 +67,11 @@ docker --version
 docker-compose --version
 
 # Image magick
-sudo apt-get update && sudo apt-get install ghostscript imagemagick
-# Check
-magick --version
+sudo apt-get update && sudo apt-get install ghostscript imagemagick && magick --version
 
 # Yarn
 # NOTE: Developer who use ubuntu >= 17.04 should run this command `sudo apt remove cmdtest` first.
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update && \
   sudo apt-get install python-software-properties && \
@@ -105,18 +79,11 @@ sudo apt-get update && \
   sudo apt-get install nodejs npm && \
   sudo apt-get install yarn
 
-sudo ln -s /usr/bin/nodejs /usr/local/bin/node
-sudo ln -s /usr/bin/npm /usr/local/bin/npm
+sudo ln -s /usr/bin/nodejs /usr/local/bin/node && sudo ln -s /usr/bin/npm /usr/local/bin/npm
 
 # Check
 node --version
 yarn --version
-
-# Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
 
 # F.lux
 sudo add-apt-repository ppa:nathan-renniewaldock/flux && sudo apt-get update && sudo apt-get install fluxgui
